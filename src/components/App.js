5dcdf2282951d8c456a52bf9cfdb14b4ab062d56
Manali Darji <darjimanali93@gmail.com>
2022-02-28 22:23:55 -0500

    Property Desc Page: implemented functionality

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import components
 import HomePage from "../pages/HomePage";
 import PropListingsPage from '../pages/PropListingsPage';
+import PropDescPage from '../pages/PropDescPage';
 
 const App = () => {
 	return (
@@ -13,6 +14,7 @@ const App = () => {
 			<Routes>
 				<Route path="/" element={<HomePage />}/>
 				<Route path="properties" element={<PropListingsPage />}/>
+				<Route path='properties/:id' element={<PropDescPage/>} />
 			</Routes>
 		</BrowserRouter>
 	)
---
+++ src/pages/PropDescPage.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import PropertyItem from '../components/PropertyItem';
+
+const PropDescPage = () => {
+	const {id} = useParams();
+	let [singleProp, setSingleProp] = useState(
+		{
+			"id": 0,
+			"image": "",
+			"title": "",
+			"description": "",
+			"price": 0,
+			"type": "",
+			"houseRules": [],
+			"amenities": [],
+			"location": "",
+			"bestSeller": false
+		}
+	);
+	
+	useEffect(() => {
+		const propApiURL = `http://localhost:8999/properties/${id}`;
+		fetch(propApiURL)
+		.then(resp => resp.json())
+		.then(data => setSingleProp(data))
+		.catch(error => console.log(error));
+	}, [id]);
+
+
+	return (
+		<div>
+		<p>Prop desc page</p>
+		<PropertyItem 						
+			id={singleProp.id}
+			singleProperty={singleProp}
+		/>
+		</div>
+	)
+}
+
+export default PropDescPage
