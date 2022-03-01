cb64fddbe13f9cb5d27215d15fb2e96f2b9ec636
Manali Darji <darjimanali93@gmail.com>
2022-02-28 22:02:38 -0500

    Header: html and css implementation completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,48 @@
+import '../assets/css/Header.css';
+import { FaSearch, FaUser } from 'react-icons/fa';
+import { Link } from "react-router-dom";
+
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+	const toggleSearch = (e) => {
+		e.preventDefault();
+		document.getElementById('search-bar-wrap').classList.toggle('mob-hide');
+	}
+
+	const toggleDropdown = (e) => {
+		e.preventDefault();
+		document.getElementById('dropdown-links').classList.toggle('hide');
+	}
+
+    return(
+        <header>
+			<div className="container">
+				<div className="header-wrap">
+					<h1 className="logo">
+						<Link to='/'>Relax-Inn</Link>
+					</h1>
+
+					<div id="search-bar-wrap" className="mob-hide">
+						<input className="search-inputbox" placeholder="Search Here.." />
+						<button>
+							<FaSearch className="fa" />
+						</button>
+					</div>
+				
+					<a href='#' className='search-icon-btn mob-show' onClick={toggleSearch}><FaSearch className="fa" /></a>
+
+					<div className="header-links-wrap">
+						<Link to='properties' className='mob-hide'>Vacation Properties</Link>
+						<a href='#' className='user-icon-btn icon-btn' onClick={toggleDropdown}><FaUser className="fa" /></a>
+						<ul id="dropdown-links" className="hide">						
+							<li><Link to='signup'>Sign Up</Link></li>
+							<li><Link to='login'>Log In</Link></li>
+							<li className='mob-show'><Link to='properties'>Vacation Properties</Link></li>
+						</ul>					
+					</div>
+				</div>
+			</div>
+		</header>
+    )
 }
 
-export default Header
+export default Header;
