fd6835e9af55a2c73c6447a508e453606e65415b
Manali Darji <darjimanali93@gmail.com>
2022-03-02 18:24:00 -0500

    home page: Prop Type section implementation completed

--- src/components/PropertyTypeLists.js
+++ src/components/PropertyTypeLists.js
@@ -1,7 +1,34 @@
+import { useState, useEffect } from 'react';
+import PropertyTypeItem from './PropertyTypeItem';
+
 const PropertyTypeLists = () => {
-  return (
-    <div>PropertyTypeLists</div>
-  )
+    const [propTypes, setPropTypes] = useState([]);
+
+    useEffect(() => {
+        const MaxNoOfPropTypes = 4;
+        const propTypesApiURL = `http://localhost:8999/propertyTypes?_limit=${MaxNoOfPropTypes}`;
+
+        fetch(propTypesApiURL)
+        .then(resp => resp.json())
+        .then(data => setPropTypes(data))
+        .catch(error => console.log(error));
+    }, []);
+    return(
+        <div className='container'>
+			<h5 className="section-title">Browse by Property Types</h5>
+
+            <div className="prop-types-wrap">
+                {propTypes.map(propType => (
+                    <PropertyTypeItem                         
+                        key={propType.id} 
+                        id={propType.id} 
+                        propType={propType}
+                    />
+                ))}
+            </div>
+
+        </div>
+    )
 }
 
-export default PropertyTypeLists
+export default PropertyTypeLists;
