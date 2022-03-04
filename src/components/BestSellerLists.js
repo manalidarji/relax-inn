cf2940d131e77b53cb6a5f96835a8b55681f5115
Manali Darji <darjimanali93@gmail.com>
2022-03-03 22:10:35 -0500

    Best Seller section:functionality and styling done

--- src/components/BestSellerLists.js
+++ src/components/BestSellerLists.js
@@ -1,7 +1,47 @@
-const BestSellerLists = () => {
-  return (
-    <div>BestSellerLists</div>
-  )
+import { useState, useEffect } from "react";
+import BestSellerItem from './BestSellerItem';
+
+const BestSellerLists = (props) => {
+    // using state for all best seller properties lists
+	let [bestSellerProps, setBestSellerProps] = useState([]);
+    
+	// get all properties
+	useEffect( () => {
+        let bestSellers = [];
+        let maxNoOfBestSellerProps = 4;
+		const allPropertiesApiURL = `http://localhost:8999/properties`;
+		fetch(allPropertiesApiURL)
+		.then( resp => resp.json())
+		.then( allProps => {
+            
+            bestSellers = allProps.filter( singleProp => {
+                if( singleProp.bestSeller && maxNoOfBestSellerProps > 0){
+                    maxNoOfBestSellerProps--;
+                    return singleProp;
+                }
+				return false;
+            } );
+            setBestSellerProps(bestSellers);
+        })
+		.catch( error => console.log(error));
+	}, []);
+
+    return(
+		<div className='container divider'>
+			<h5 className="section-title">Our Best Seller</h5>
+
+            <div className="best-sellers-wrap">
+                {bestSellerProps.map(singleProperty => (
+                    <BestSellerItem                         
+                        key={singleProperty.id} 
+                        id={singleProperty.id} 
+                        singleProp={singleProperty}
+                    />
+                ))}
+            </div>
+
+        </div>
+    )
 }
 
-export default BestSellerLists
+export default BestSellerLists;
